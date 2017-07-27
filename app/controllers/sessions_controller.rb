a2c540a60436112369b6269400562de17e7d8e8f
Kathleen <kathleenmcclay@gmail.com>
2017-07-27 12:14:14 -0400

    Added Credits and skeleton for logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    u = User.find_by(email: params[:session][:email])
+
+    if u && u.authenticate(params[:session][:password])
+
+      flash[:notice] = ' Succeful! You are logeed in! '
+      redirect_to_root_url
+
+    else
+      flast.now[:alert] = ' Try again!'
+      render :new
+    end
   end
 
   def destroy
